3f20d44cd474ba75c02a1b35fa585bfddfe913c9
GerardPhillips <32836499+GerardPhillips@users.noreply.github.com>
2018-04-23 10:40:06 +0100

    Add usage info

--- sFlowAnalyser.py
+++ sFlowAnalyser.py
@@ -3,3 +3,10 @@
 ################################    
 import sys
 import fileinput
+
+# usage info:
+# sFlowAnalyser [optional filename]
+# if no filename is provided, sFlowAnalyser will read from stdin
+# filename option is useful for testing, code development etc
+# Usual use would be to pipe the output of sflowTool to the sFlow Analyser:
+# sFlowTool > python sFlowAnalyser.py
